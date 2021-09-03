7dc0b9991e511e9abb23735a9a5c0f3ed174162c
muhammad-khair <muhammad_khair@outlook.com>
2021-09-04 02:15:00 +0800

    Update resume hyperlink

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -54,7 +54,7 @@ export default {
                 {
                     name: 'Resume',
                     opensNewTab: true,
-                    link: 'https://github.com/muhammad-khair/muhammad-khair.github.io/blob/main/public/resume.pdf',
+                    link: 'https://github.com/muhammad-khair/muhammad-khair.github.io/blob/main/src/assets/resume.pdf',
                     class: 'split ',
                     svg: 'M4.5 3a2.5 2.5 0 0 1 5 0v9a1.5 1.5 0 0 1-3 0V5a.5.5 0 0 1 1 0v7a.5.5 0 0 0 1 0V3a1.5 1.5 0 1 0-3 0v9a2.5 2.5 0 0 0 5 0V5a.5.5 0 0 1 1 0v7a3.5 3.5 0 1 1-7 0V3z'
                 }
