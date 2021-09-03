f7e8d8238e8efbe32b59cceac5228a2f5983eea2
muhammad-khair <muhammad_khair@outlook.com>
2021-09-03 17:14:08 +0800

    Style navbar and footer

--- src/App.vue
+++ src/App.vue
@@ -21,9 +21,6 @@ export default {
 
 <style>
 body {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
 }
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="navbar navbar-expand-lg navbar-light bg-light" id="contact">
+    <div class="navbar navbar-expand-lg navbar-dark bg-dark" id="contact">
         <div>
-            <h3 style="text-align: center;">Let's connect!</h3>
+            <h3 class="text-white bg-dark">Let's connect!</h3>
         </div>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav ml-auto">
                 <li class="nav-item" v-for="contact in contacts" :key="contact">
                     <a class="nav-link" :href="contact.link" target="_blank" rel="noopener noreferrer">{{ contact.name }}</a>
                 </li>
             </ul>
-            <a href="#" class="float-right">Jump back to top</a>
+            <a href="#" class="nav-link text-light">Jump back to top</a>
         </div>
     </div>
 </template>
